f372fd449f753b2ca031779cc65aeca4cb100a71
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-17 11:12:17 +0100

    Finished page design
    Added html and css which will be used to represent
    the page, as well as some javascript to represent
    the score and high score respectively.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,112 @@
 <script setup>
 import { ref } from 'vue';
 
-const count = ref(0);
+const highScore = ref(0);
+
+const score = ref(0);
 
 </script>
 
 <template>
-    <p>{{count}}</p>
-    <button @click="count++">Increment</button>
+    <header>
+        <h1>Number Guessing Game</h1>
+    </header>
+
+    <main>
+        <p class="high-score">High Score: {{highScore}}</p>
+        <p class="score">Score: {{score}}</p>
+
+        <form class="guessing-form">
+            <input class="guessing-input" type="number" name="user-guess" id="user-guess">
+            <button class="guessing-button">Guess</button>
+        </form>
+    </main>
 </template>
 
 <style scoped>
-    p {
-        color: red;
+    header > h1 {
+        color: var(--text-color);
+        font-size: 3rem;
+        margin-top: 3rem;
+        text-align: center;
     }
 
-    button {
+    main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
+
+    main > p {
+        color: var(--text-color);
+        text-align: center;
+    }
+
+    .high-score {
+        margin-top: 3rem;
+        font-size: 2rem;
+    }
+
+    .score {
+        margin-top: 5rem;
+        font-size: 1.5rem;
+        animation-duration: .7s;
+    }
+
+    @keyframes correct {
+        50% {
+            color: var(--correct-score-color);
+            font-size: 2.5rem;
+        }
+    }
+
+    @keyframes wrong {
+        25% {
+            color: var(--wrong-score-color);
+            font-size: 2.5rem;
+            transform: translateX(-3.5rem);
+        }
+        50% {
+            color: var(--wrong-score-color);
+            font-size: 2.5rem;
+            transform: translateX(3.5rem);
+        }
+        75% {
+            color: var(--wrong-score-color);
+            font-size: 2.5rem;
+            transform: translateX(-3.5rem);
+        }
+        100% {
+            transform: translateX(0);
+            color: var(--wrong-score-color);
+        }
+    }
+
+    .guessing-form {
+        margin: 5rem auto;
+        width: min(100% - 2rem, 600px);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .guessing-input,
+    .guessing-button {
+        padding: 4rem;
+        font-size: 3rem;
+        border: none;
+        outline: none;
+    }
+
+    .guessing-input {
+        border-radius: .5rem .5rem 0 0;
+        text-align: center;
+    }
+
+    .guessing-button {
+        border-radius: 0 0 .5rem .5rem;
         cursor: pointer;
     }
 </style>
