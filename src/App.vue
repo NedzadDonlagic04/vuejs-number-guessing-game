b6cfcfe8d1b15aa9ea542061d64ac3e5a70c6bf1
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-17 09:45:48 +0100

    Added basic click counter for testing

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+</script>
+
+<template>
+    <p>{{count}}</p>
+    <button @click="count++">Increment</button>
+</template>
+
+<style scoped>
+    p {
+        color: red;
+    }
+
+    button {
+        cursor: pointer;
+    }
+</style>
